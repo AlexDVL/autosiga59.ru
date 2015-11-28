cdd1c8a2a580747608f2940b23d93a2d390d71da
AlexDVL <alexandr32167@gmail.com>
2015-11-28 16:21:08 +0500

    Update

---
+++ www/src/js/files.js
@@ -0,0 +1,16 @@
+/*
+ * Third Party
+ */
+
+//= thirdParty/jquery-1.11.3.js
+//= thirdParty/history.js
+//= thirdParty/jquery.colorbox.js
+
+
+
+/*
+ * Development
+ */
+
+//= ajax.js
+//= settings.js
